3a9ca05400aa4d683c9be5fe5ace26c9b0704076
Alper KILIÇKAYA <alperkilickaya@gmail.com>
2021-09-13 15:15:40 +0300

    basename changed

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 
 const AppRouter = () => {
     return(
-        <Router basename="alper">
+        <Router basename="tatangalar">
             <Navbar />
         </Router>
     )
